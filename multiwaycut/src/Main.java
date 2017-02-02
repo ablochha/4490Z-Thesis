30797670b345968b8e1f8326701adce0224d0d3c
Andrew Bloch-Hansen <ablochha@uwo.ca>
2017-02-01 21:00:25 -0500

    Added the main file

---
+++ multiwaycut/src/Main.java
@@ -0,0 +1,44 @@
+import algorithms.LocalSearch;
+import library.In;
+import library.StdOut;
+import algorithms.IsolationHeuristic;
+
+/**
+ * Created by Bloch-Hansen on 2017-01-23.
+ */
+public class Main {
+
+    public static final String EXAMPLE1 = "data" + System.getProperty("file.separator") + "testnetwork1.txt";
+    public static final String EXAMPLE2 = "data" + System.getProperty("file.separator") + "testnetwork2.txt";
+
+    public static void main(String[] args) {
+
+        IsolationHeuristic isolationHeuristic = new IsolationHeuristic();
+        LocalSearch localSearch = new LocalSearch();
+
+        int ihCost = 0;
+        int lsCost = 0;
+
+        // Run the example graph
+        try {
+
+            In in = new In(EXAMPLE2);
+            ihCost = isolationHeuristic.computeMultiwayCut(in);
+
+            in = new In(EXAMPLE2);
+            //lsCost = localSearch.computeMultiwayCut(in);
+
+            //StdOut.println("Isolation Heuristic: " + ihCost + ", Local Search: " + lsCost);
+
+        } //end try
+
+        // The file didn't work
+        catch (Exception e) {
+
+            StdOut.println("The graph failed, reason: " + e);
+
+        } //end catch
+
+    } //end main
+
+} //end Main
