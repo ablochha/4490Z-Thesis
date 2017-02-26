08900b2ab6b72705bbf52010c1e0bd255816d04e
Andrew Bloch-Hansen <ablochha@uwo.ca>
2017-02-26 17:25:12 -0500

    Moved test filenames to a properties file

--- multiwaycut/src/Main.java
+++ multiwaycut/src/Main.java
@@ -5,65 +5,27 @@ import library.StdOut;
 import algorithms.IsolationHeuristic;
 import utility.ConnectedComponentSearcher;
 import utility.GraphFormatReader;
+import utility.PropertiesReader;
 
 /**
  * Created by Bloch-Hansen on 2017-01-23.
  */
 public class Main {
 
-    private static final String SLASH = System.getProperty("file.separator");
-
-    private static final String EXAMPLE1 = "data" + SLASH + "testnetwork1.txt";
-    private static final String EXAMPLE2 = "data" + SLASH + "testnetwork2.txt";
-
-    private static final String FIVEBYFIVE_1 = "data" + SLASH + "5x5_1.txt";
-    private static final String FIVEBYFIVE_2 = "data" + SLASH + "5x5_2.txt";
-    private static final String FIVEBYFIVE_3 = "data" + SLASH + "5x5_3.txt";
-    private static final String FIVEBYFIVE_4 = "data" + SLASH + "5x5_4.txt";
-    private static final String FIVEBYFIVE_5 = "data" + SLASH + "5x5_5.txt";
-    private static final String FIVEBYFIVE_6 = "data" + SLASH + "5x5_6.txt";
-
-    private static final String C125_9_1 = "data" + SLASH + "challenge2" + SLASH + "c125_9_1.txt";
-    private static final String C125_9_2 = "data" + SLASH + "challenge2" + SLASH + "c125_9_2.txt";
-    private static final String C125_9_1_EDGES = "data" + SLASH + "challenge2" + SLASH + "c125_9_1_edges.txt";
-    private static final String C125_9_2_EDGES = "data" + SLASH + "challenge2" + SLASH + "c125_9_2_edges.txt";
-
-    private static final String BROCK200_2_1 = "data" + SLASH + "challenge2" + SLASH + "brock200_2_1.txt";
-    private static final String BROCK200_2_2 = "data" + SLASH + "challenge2" + SLASH + "brock200_2_2.txt";
-    private static final String BROCK200_2_1_EDGES = "data" + SLASH + "challenge2" + SLASH + "brock200_2_1_edges.txt";
-    private static final String BROCK200_2_2_EDGES = "data" + SLASH + "challenge2" + SLASH + "brock200_2_2_edges.txt";
-
-    private static final String P_HAT300_1_1 = "data" + SLASH + "challenge2" + SLASH + "p_hat300_1_1.txt";
-    private static final String P_HAT300_1_2 = "data" + SLASH + "challenge2" + SLASH + "p_hat300_1_2.txt";
-    private static final String P_HAT300_1_1_EDGES = "data" + SLASH + "challenge2" + SLASH + "p_hat300_1_1_edges.txt";
-    private static final String P_HAT300_1_2_EDGES = "data" + SLASH + "challenge2" + SLASH + "p_hat300_1_2_edges.txt";
-
-    private static final String DIMACS_FRB30_15_1_1 = "data" + SLASH + "frb30-15" + SLASH + "frb30-15-1_1.txt";
-    private static final String DIMACS_FRB30_15_1_2 = "data" + SLASH + "frb30-15" + SLASH + "frb30-15-1_2.txt";
-    private static final String DIMACS_FRB30_15_1_1_EDGES = "data" + SLASH + "frb30-15" + SLASH + "frb30-15-1_1_edges.txt";
-    private static final String DIMACS_FRB30_15_1_2_EDGES = "data" + SLASH + "frb30-15" + SLASH + "frb30-15-1_2_edges.txt";
-
-    private static final String P_HAT700_1_1 = "data" + SLASH + "challenge2" + SLASH + "p_hat700_1_1.txt";
-    private static final String P_HAT700_1_2 = "data" + SLASH + "challenge2" + SLASH + "p_hat700_1_2.txt";
-    private static final String P_HAT700_1_1_EDGES = "data" + SLASH + "challenge2" + SLASH + "p_hat700_1_1_edges.txt";
-    private static final String P_HAT700_1_2_EDGES = "data" + SLASH + "challenge2" + SLASH + "p_hat700_1_2_edges.txt";
-
-    private static final String DIMACS_FRB59_26_1_1 = "data" + SLASH + "frb59-26" + SLASH + "frb59-26-1_1.txt";
-    private static final String DIMACS_FRB59_26_1_2 = "data" + SLASH + "frb59-26" + SLASH + "frb59-26-1_2.txt";
-    private static final String DIMACS_FRB59_26_1_1_EDGES = "data" + SLASH + "frb59-26" + SLASH + "frb59-26-1_1_edges.txt";
-    private static final String DIMACS_FRB59_26_1_2_EDGES = "data" + SLASH + "frb59-26" + SLASH + "frb59-26-1_2_edges.txt";
-
-    private static final String CURRENT = DIMACS_FRB30_15_1_2;
+    private static final String CURRENT = "I080_345";
 
     public static void main(String[] args) {
+
+        PropertiesReader prop = new PropertiesReader();
+
+        GraphFormatReader reader = new GraphFormatReader();
+        ConnectedComponentSearcher searcher = new ConnectedComponentSearcher();
 
         IsolationHeuristic isolationHeuristic = new IsolationHeuristic();
         LocalSearch localSearch = new LocalSearch();
         MultiwayCutSolver solver = new MultiwayCutSolver();
-        GraphFormatReader reader = new GraphFormatReader();
-        ConnectedComponentSearcher searcher = new ConnectedComponentSearcher();
 
-        FlowNetwork largestConnectedComponent = searcher.getLargestConnectedComponent(reader.parse(CURRENT));
+        FlowNetwork largestConnectedComponent = searcher.getLargestConnectedComponent(reader.parse(prop.get(CURRENT)));
 
         int ihCost = isolationHeuristic.computeMultiwayCut(new FlowNetwork(largestConnectedComponent));
         int lsCost = localSearch.computeMultiwayCut(new FlowNetwork(largestConnectedComponent));
---
+++ multiwaycut/src/utility/PropertiesReader.java
@@ -0,0 +1,54 @@
+package utility;
+
+import library.In;
+import library.StdOut;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by Bloch-Hansen on 2017-02-26.
+ */
+public class PropertiesReader {
+
+    private InputStream input = null;
+    private Properties prop = new Properties();
+
+    public String get(String name) {
+
+        try {
+
+            input = getClass().getClassLoader().getResourceAsStream("benchmarks.properties");
+            prop.load(input);
+            return prop.getProperty(name);
+
+        } catch (IOException ex) {
+
+            ex.printStackTrace();
+
+        } finally{
+
+            if (input != null) {
+
+                try {
+
+                    input.close();
+
+                } catch (IOException e) {
+
+                    e.printStackTrace();
+
+                }
+
+            }
+
+        }
+
+        return null;
+
+    } //end get
+
+} //end PropertiesReader
