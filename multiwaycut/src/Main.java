c5f63cd3c50ac3fdc20cc14a744102d8ea87cc40
Andrew Bloch-Hansen <ablochha@uwo.ca>
2017-05-19 22:03:46 -0400

    Added ability to conduct multiple repititions for the randomized algorithms

--- multiwaycut/src/Main.java
+++ multiwaycut/src/Main.java
@@ -1,5 +1,6 @@
 import algorithms.*;
 import cplex.MultiwayCutSolver;
+import cplex.Simplex;
 import datastructures.flownetwork.FlowNetwork;
 import library.StdOut;
 import utility.*;
@@ -11,7 +12,12 @@ public class Main {
 
     private static final String CURRENT = "GE080";
     private static final String OUT_NAME = "-999.txt";
-    private static final int NUM_TRIALS = 10;
+
+    private static final int NUM_ALGORITHMS = 13;
+    private static final int NUM_TRIALS = 1000;
+    private static final int NUM_REPITITIONS = 1;
+
+    private static final int EARLY_EXIT = 5;
 
     private static final double EPSILON1 = 1000.0;
     private static final double EPSILON2 = 1.6;
@@ -19,62 +25,135 @@ public class Main {
     private static final double EPSILON4 = .315;
     private static final double EPSILON5 = .16;
 
+    private static final double THRESHOLD = 1.05;
+
     public static void main(String[] args) {
 
-        TestObject[] tests = new TestObject[9];
+        TestObject[] tests = new TestObject[NUM_ALGORITHMS];
 
         PropertiesReader prop = new PropertiesReader();
 
         GraphFormatReader reader = new GraphFormatReader();
         ConnectedComponentSearcher searcher = new ConnectedComponentSearcher();
 
+        MultiwayCutStrategy solver = new Simplex();
+        MultiwayCutStrategy ih = new IsolationHeuristic();
+
+        int repititions;
         int trials = 0;
         int vertices = 0;
+        int k = 0;
+
+        double ratio;
         double edgesSum = 0.0;
 
-        tests[0] = new TestObject(NUM_TRIALS, "Cplex", new MultiwayCutSolver());
-        tests[1] = new TestObject(NUM_TRIALS, "Isolation Heuristic", new IsolationHeuristic());
-        tests[2] = new TestObject(NUM_TRIALS, "Local Search (epsilon = " + EPSILON1 + ")", new LocalSearch(), EPSILON1);
-        tests[3] = new TestObject(NUM_TRIALS, "Local Search (epsilon = " + EPSILON2 + ")", new LocalSearch(), EPSILON2);
-        tests[4] = new TestObject(NUM_TRIALS, "Local Search (epsilon = " + EPSILON3 + ")", new LocalSearch(), EPSILON3);
-        tests[5] = new TestObject(NUM_TRIALS, "Local Search (epsilon = " + EPSILON4 + ")", new LocalSearch(), EPSILON4);
-        tests[6] = new TestObject(NUM_TRIALS, "Local Search (epsilon = " + EPSILON5 + ")", new LocalSearch(), EPSILON5);
-        tests[7] = new TestObject(NUM_TRIALS, "Calinescu", new Calinescu(), new MultiwayCutSolver());
-        tests[8] = new TestObject(NUM_TRIALS, "Buchbinder", new Buchbinder(), new MultiwayCutSolver());
+        boolean flag;
+
+        tests[0] = new TestObject("Cplex", new MultiwayCutSolver());
+        tests[1] = new TestObject("Simplex", solver);
+        tests[2] = new TestObject("Isolation Heuristic", ih);
+        tests[3] = new TestObject("Local Search (epsilon = " + EPSILON2 + ", OneEach)", new LocalSearch(), "ONEEACH", EPSILON2, ih);
+        tests[4] = new TestObject("Local Search (epsilon = " + EPSILON2 + ", Clumps)", new LocalSearch(), "CLUMPS", EPSILON2, ih);
+        tests[5] = new TestObject("Local Search (epsilon = " + EPSILON2 + ", Random)", new LocalSearch(), "RANDOM", EPSILON2, ih);
+        tests[6] = new TestObject("Local Search (epsilon = " + EPSILON2 + ", Random)", new LocalSearch(), "RANDOM", EPSILON2, ih);
+        tests[7] = new TestObject("Local Search (epsilon = " + EPSILON2 + ", Isolation Heuristic)", new LocalSearch(), "ISOLATION HEURISTIC", EPSILON2, ih);
+        tests[8] = new TestObject("Calinescu", new Calinescu(), solver);
+        tests[9] = new TestObject("Exponential Clocks", new ExponentialClocks(), solver);
+        tests[10] = new TestObject("Buchbinder", new Buchbinder(), solver);
+        tests[11] = new TestObject("Descending Threshold", new DescendingThreshold(), solver);
+        tests[12] = new TestObject("Independent Threshold", new IndependentThreshold(), solver);
 
         while (trials < NUM_TRIALS) {
+
+            repititions = 0;
+            flag = false;
+            trials++;
+
+            StdOut.println("****************************************Trial " + trials + " of " + NUM_TRIALS + "****************************************");
 
             FlowNetwork largestConnectedComponent = searcher.getLargestConnectedComponent(reader.parse(prop.get(CURRENT), OUT_NAME));
 
             vertices = largestConnectedComponent.getNumVertices();
             edgesSum += largestConnectedComponent.getNumEdges();
+            k = largestConnectedComponent.getK();
 
-            tests[0].run(largestConnectedComponent);
+            tests[0].run(new FlowNetwork(largestConnectedComponent), largestConnectedComponent.getNumVertices(), largestConnectedComponent.getNumEdges(), reader.getCapacity1(), reader.getCapacity2());
+            tests[1].run(new FlowNetwork(largestConnectedComponent), largestConnectedComponent.getNumVertices(), largestConnectedComponent.getNumEdges(), reader.getCapacity1(), reader.getCapacity2());
 
-            for (int i = 1; i < tests.length; i++) {
+            while (repititions < NUM_REPITITIONS) {
 
-                tests[i].run(largestConnectedComponent);
-                tests[i].update(tests[0].getCost());
+                repititions++;
+                StdOut.println("****************************************Repitition " + repititions + " of " + NUM_REPITITIONS + "(" + trials + "/" + NUM_TRIALS + ")****************************************");
 
-            } //end for
+                for (int i = 2; i < NUM_ALGORITHMS; i++) {
 
-            trials++;
+                    if (repititions == 1) {
+
+                        tests[i].run(new FlowNetwork(largestConnectedComponent), largestConnectedComponent.getNumVertices(), largestConnectedComponent.getNumEdges(), reader.getCapacity1(), reader.getCapacity2());
+                        ratio = tests[i].update(tests[0].getCost());
+
+                        if (i >= 8 && ratio > THRESHOLD) {
+
+                            flag = true;
+
+                        } //end if
+
+                    } //end if
+
+                    else {
+
+                        if (i >= 8) {
+
+                            tests[i].run(new FlowNetwork(largestConnectedComponent), largestConnectedComponent.getNumVertices(), largestConnectedComponent.getNumEdges(), reader.getCapacity1(), reader.getCapacity2());
+                            ratio = tests[i].update(tests[0].getCost());
+
+                            if (ratio > THRESHOLD) {
+
+                                flag = true;
+
+                            } //end if
+
+                        } //end if
+
+                    } //end else
+
+                } //end for
+
+                if (repititions >= EARLY_EXIT && !flag) {
+
+                    break;
+
+                } //end if
+
+            } //end while
+
+            if (repititions >= EARLY_EXIT && flag) {
+
+                tests[8].outputRadiusRatio(reader);
+                tests[10].outputRadiusRatio(reader);
+                tests[12].outputRadiusRatio(reader);
+
+            } //end if
+
+            tests[8].clearRadiusRatio();
+            tests[10].clearRadiusRatio();
+            tests[12].clearRadiusRatio();
 
         } //end while
 
         // Update the test objects statistics
-        for (int i = 1; i < tests.length; i++) {
+        for (int i = 2; i < NUM_ALGORITHMS; i++) {
 
             tests[i].calculate();
 
         } //end for
 
-        StdOut.println("\nTrials: " + trials + ", Vertices: " + vertices + ", Edges: " + (int) (edgesSum / trials));
+        StdOut.println("\nTrials: " + trials + ", Vertices: " + vertices + ", Edges: " + (int) (edgesSum / trials) + ", K: " + k);
 
         // Output the test objects results
-        for (int i = 1; i < tests.length; i++) {
+        for (int i = 2; i < NUM_ALGORITHMS; i++) {
 
-            tests[i].output();
+            tests[i].output(reader);
 
         } //end for
 
